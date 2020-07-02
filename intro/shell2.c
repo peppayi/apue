90da8ccf4078e12a46534b7370c50b50c9d761f2
hacker <hacker@debian.os.local>
2020-07-02 09:11:32 -0400

    demo - signal catching

---
+++ intro/shell2.c
@@ -0,0 +1,42 @@
+#include "apue.h"
+#include <sys/wait.h>
+
+static void sig_init(int);   /* our signal-catching function */
+
+int main(int argc, char *argv[])
+{
+    char buf[MAXLINE];
+    pid_t pid;
+    int status;
+
+    if (signal(SIGINT, sig_init) == SIG_ERR)
+        err_sys("signal error");
+
+    printf("%% ");
+    while (fgets(buf, MAXLINE, stdin) != NULL) {
+        if (buf[strlen(buf)-1] == '\n')
+            buf[strlen(buf)-1] = 0;
+
+        if ((pid = fork()) < 0) {
+            err_sys("fork error");
+        }
+        /* child process */
+        else if (pid == 0) {
+            execlp(buf, buf, (char *)0);
+            err_ret("couldn't execute: %s", buf);
+            exit(127);
+        }
+
+        /* parent */
+        if ((pid = waitpid(pid, &status, 0)) < 0)
+            err_sys("waitpid error");
+        printf("%% ");
+    }
+
+    exit(0);
+}
+
+void sig_init(int signo)
+{
+    printf("interrupt\n%%");
+}
